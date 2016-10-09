7398af2f0303eb50bb46cd573fb75d5c2addb17f
Brian DeMaio <Brian DeMaio>
2016-10-08 21:13:49 -0500

    initial server stuff

---
+++ lightstripping-server/Server.py
@@ -0,0 +1,61 @@
+#!/usr/bin/env python
+"""
+Very simple HTTP server in python.
+
+Usage::
+    ./dummy-web-server.py [<port>]
+
+Send a GET request::
+    curl http://localhost
+
+Send a HEAD request::
+    curl -I http://localhost
+
+Send a POST request::
+    curl -d "foo=bar&bin=baz" http://localhost
+
+"""
+from http.server import BaseHTTPRequestHandler, HTTPServer
+import socketserver
+import RPi.GPIO as GPIO
+import time
+
+class S(BaseHTTPRequestHandler):
+    def _set_headers(self):
+        self.send_response(200)
+        self.send_header('Content-type', 'text/html')
+        self.end_headers()
+
+    def do_GET(self):
+        self._set_headers()
+        self.wfile.write(bytes("<html><body><h1>hi!</h1></body></html>", "utf8"))
+        print "LED on"
+        GPIO.output(18,GPIO.HIGH)
+        time.sleep(1)
+        print "LED off"
+        GPIO.output(18,GPIO.LOW)
+
+    def do_HEAD(self):
+        self._set_headers()
+        
+    def do_POST(self):
+        # Doesn't do anything with posted data
+        self._set_headers()
+        self.wfile.write(bytes("<html><body><h1>POST!</h1></body></html>", "utf8"))
+        
+def run(server_class=HTTPServer, handler_class=S, port=80):
+    server_address = ('', port)
+    httpd = server_class(server_address, handler_class)
+    print('Starting httpd...')
+    GPIO.setmode(GPIO.BCM)
+    GPIO.setwarnings(False)
+    GPIO.setup(18,GPIO.OUT)
+    httpd.serve_forever()
+
+if __name__ == "__main__":
+    from sys import argv
+
+    if len(argv) == 2:
+        run(port=int(argv[1]))
+    else:
+        run()
